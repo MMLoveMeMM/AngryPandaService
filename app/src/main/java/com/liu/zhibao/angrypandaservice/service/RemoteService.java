7e32d59d68e5eb56ee9610c71c50c605351f6cb5
liuzhibao <liuzhibao@xl.cn>
2018-08-15 11:14:53 +0800

    远程服务对调用者的连接使用限制,加权限处理

--- app/src/main/java/com/liu/zhibao/angrypandaservice/service/RemoteService.java
+++ app/src/main/java/com/liu/zhibao/angrypandaservice/service/RemoteService.java
@@ -1,8 +1,12 @@
 package com.liu.zhibao.angrypandaservice.service;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Binder;
 import android.os.IBinder;
+import android.os.Parcel;
 import android.os.RemoteException;
 import android.util.Log;
 import android.widget.Toast;
@@ -23,10 +27,51 @@ import org.jetbrains.annotations.Nullable;
 public class RemoteService extends Service {
 
     private final static String TAG = RemoteService.class.getName();
+
+    private final static String PERMISSION_NAME = "com.liu.zhibao.ACCESS_REMOTE_SERVICE";
+    private final static String CALLING_PKG_NAME="com.liu.zhibao.angrypandaservice";
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return stub;
+
+        Log.e(TAG,"pkg name : "+intent.getComponent().getPackageName());
+        String callingPkgName = intent.getComponent().getPackageName();
+        if(checkPermission(getApplicationContext(),PERMISSION_NAME,callingPkgName/* 客户端(连接服务者)的包名 */)) {
+            /*
+            * 有指定权限的APP 才能够连接到这个服务,否则不准对方连接
+            * */
+            return stub;
+        }else{
+            /*
+            * 无权限禁止连接
+            * */
+            return null;
+        }
+    }
+
+    // 这个方法不管用,无法通过检查被调用者的权限来判断调用者是否有权限
+    public boolean checkPermission(Context context, String permission) {
+        //检查如果是当前应用则返回真
+        permission="com.liu.zhibao.ACCESS_REMOTE_SERVICE";
+        if (context.checkCallingPermission(permission) == PackageManager.PERMISSION_DENIED) {
+            Log.e(TAG,"client has no permission : " + permission);
+            return false;
+        }
+        return true;
+    }
+
+    /*
+    * 还是需要通过检查对应连接者的包名来检查这个连接者是否有制定的权限
+    * */
+    private boolean checkPermission(Context context, String permName, String pkgName){
+        PackageManager pm = context.getPackageManager();
+        if(PackageManager.PERMISSION_GRANTED == pm.checkPermission(permName, pkgName)){
+            Log.e(TAG,pkgName + " has permission : " + permName);
+            return true;
+        }else{
+            Log.e(TAG,pkgName + "has no permission : " + permName);
+            return false;
+        }
     }
 
     private IRemoteServiceInteface.Stub stub=new IRemoteServiceInteface.Stub(){
@@ -38,8 +83,25 @@ public class RemoteService extends Service {
             Log.e(TAG,"info : "+data);
         }
 
-
-
+        @Override
+        public boolean onTransact(int code, Parcel data, Parcel reply, int flags) throws RemoteException {
+            Log.e(TAG,"过滤器filter code:" + code + "---flags:" + flags);
+            String packageName = null;
+            String[] packages = getPackageManager().
+                    getPackagesForUid(getCallingUid());
+            if (packages != null && packages.length > 0) {
+                packageName = packages[0];
+            }
+            if (packageName == null) {
+                return false;
+            }
+            boolean checkPermission = checkPermission(RemoteService.this, PERMISSION_NAME, packageName);
+            if (!checkPermission) {
+                Log.e(TAG,"onTransact check permission : no permission !");
+                return false;
+            }
+            return super.onTransact(code, data, reply, flags);
+        }
     };
 
     @Override
